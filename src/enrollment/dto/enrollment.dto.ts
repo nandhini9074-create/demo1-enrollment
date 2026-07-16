700c82d10b26d865da5c8f8df5ed5093dca6f77a
NANDHINI S <nandhini9074@gmail.com>
2026-07-16 11:48:54 +0530

    chore: add src/enrollment/dto/enrollment.dto.ts

---
+++ src/enrollment/dto/enrollment.dto.ts
@@ -0,0 +1,38 @@
+```typescript
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
+
+export class FileDto {
+  @ApiProperty({ example: 'encrypted-data' })
+  @IsString()
+  data: string;
+
+  @ApiProperty({ example: '2026-05-20T10:00:00Z' })
+  @IsString()
+  fileDateTime: string;
+
+  @ApiProperty({ example: 'enrolment-file.csv' })
+  @IsString()
+  fileName: string;
+
+  @ApiProperty({ example: 1 })
+  @IsNumber()
+  fileRecordNum: number;
+}
+
+export class FileStatusDto {
+  @ApiProperty({ example: 'enrolment-file.csv' })
+  @IsString()
+  fileName: string;
+
+  @ApiProperty({ example: 10 })
+  @IsNumber()
+  fileRecordNum: number;
+}
+
+export class GetFileStatusDto {
+  @ApiProperty({ example: 'enrolment-file.csv' })
+  @IsString()
+  fileName: string;
+}
+```
